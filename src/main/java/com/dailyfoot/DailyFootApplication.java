267b3efc838ef62bdd2a1bcfecc1f7f12a7545e8
Chebbi <said.chebbi41@gmail.com>
2025-08-21 11:54:53 +0200

    Initialisation du back du projet DailyFoot

---
+++ src/main/java/com/dailyfoot/DailyFootApplication.java
@@ -0,0 +1,13 @@
+package com.dailyfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyFootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyFootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailyfoot/DailyFootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyfoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyFootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
