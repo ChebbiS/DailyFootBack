b26d194a3b013cdfe22f0ebbd7890123de0144c0
Chebbi <said.chebbi41@gmail.com>
2025-08-25 16:40:00 +0200

    Fin UserService

---
+++ src/main/java/com/dailyfoot/services/AgentService.java
@@ -0,0 +1,7 @@
+package com.dailyfoot.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgentService {
+}
--- src/main/java/com/dailyfoot/services/UserService.java
+++ src/main/java/com/dailyfoot/services/UserService.java
@@ -1,8 +1,43 @@
 package com.dailyfoot.services;
 
+import com.dailyfoot.entities.User;
+import com.dailyfoot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
-    
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findAll()
+                .stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst();
+    }
 }
