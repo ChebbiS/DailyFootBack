500a4407ba50cda62d057e611b5b15ba1aefeca6
Chebbi <said.chebbi41@gmail.com>
2025-08-25 16:50:22 +0200

    StatistiqueService Fini

--- src/main/java/com/dailyfoot/services/StatistiqueService.java
+++ src/main/java/com/dailyfoot/services/StatistiqueService.java
@@ -1,7 +1,43 @@
 package com.dailyfoot.services;
 
+import com.dailyfoot.entities.Statistique;
+import com.dailyfoot.repositories.StatistiqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StatistiqueService {
+
+    private final StatistiqueRepository statistiqueRepository;
+
+    @Autowired
+    public StatistiqueService(StatistiqueRepository statistiqueRepository) {
+        this.statistiqueRepository = statistiqueRepository;
+    }
+
+    public List<Statistique> getAllStatistiques() {
+        return statistiqueRepository.findAll();
+    }
+
+    public Optional<Statistique> getStatistiqueById(Integer id) {
+        return statistiqueRepository.findById(id);
+    }
+
+    public Statistique saveStatistique(Statistique statistique) {
+        return statistiqueRepository.save(statistique);
+    }
+
+    public void deleteStatistique(Integer id) {
+        statistiqueRepository.deleteById(id);
+    }
+
+    public Optional<Statistique> getStatistiqueByPlayerId(Integer id) {
+        return statistiqueRepository.findAll()
+                .stream()
+                .filter(statistique -> statistique.getPlayer().getPlayerId() == id)
+                .findFirst();
+    }
 }
