7cce9cdbab8add0fc1e7fd7c23111093ee096b43
Yusuf Karaca <karacayif@gmail.com>
2025-09-04 14:30:54 +0200

    Requête getAllStats ready

--- src/main/java/com/dailyfoot/controllers/StatistiqueController.java
+++ src/main/java/com/dailyfoot/controllers/StatistiqueController.java
@@ -6,17 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/statistique")
 public class StatistiqueController {
+
     private final StatistiqueService statistiqueService;
+
     @Autowired
     public StatistiqueController(StatistiqueService statistiqueService) {
         this.statistiqueService = statistiqueService;
     }
-    @PatchMapping("/update/{id}")
+
+    @GetMapping //http://localhost:8080/statistique
+    public ResponseEntity<List<StatistiqueDTO>>getAllStatistiques() {
+        List<StatistiqueDTO> stats = statistiqueService.getAllStatistiques();
+        return ResponseEntity.ok(stats);
+    }
+
+
+    @PatchMapping("/update/{id}") //https://localhost:8080/statistique/update/1
     public ResponseEntity<StatistiqueDTO> updateStatistiques(
             @PathVariable int id,
             @RequestBody Map<String, Object> updates) {
